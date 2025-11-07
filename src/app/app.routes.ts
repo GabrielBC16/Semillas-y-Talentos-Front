15ec8d788afa1a670ba97ac93b335ee5d4f2dde8
Gabriel Barrantes <137751722+GabrielBC16@users.noreply.github.com>
2025-11-07 08:55:10 -0500

    update route v2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,25 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home.component/home.component';
-import { AboutComponent } from './pages/about.component/about.component';
-import { ProjectsComponent } from './pages/projects.component/projects.component';
-import { ContactComponent } from './pages/contact.component/contact.component';
-import { DonationsComponent } from './pages/donations.component/donations.component';
+  import { Routes } from '@angular/router';
+  import { HomeComponent } from './pages/home.component/home.component';
+  import { AboutComponent } from './pages/about.component/about.component';
+  import { ProjectsComponent } from './pages/projects.component/projects.component';
+  import { ContactComponent } from './pages/contact.component/contact.component';
+  import { DonationsComponent } from './pages/donations.component/donations.component';
+import { RenderMode } from '@angular/ssr';
 
-export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'projects', component: ProjectsComponent },
-  {
-    path: 'projects/:id',
-
-    loadComponent: () =>
-      import(
-        './pages/projects.component/projects-details.component/projects-details.component'
-      ).then((m) => m.ProjectsDetailsComponent),
-      data: {prerender: false}
-  },
-  { path: 'contact', component: ContactComponent },
-  { path: 'donations', component: DonationsComponent },
-  { path: 'projects/charla-micaela-bastidas', component: HomeComponent },
-];
+  export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'projects', component: ProjectsComponent },
+    {
+      path: 'projects/:id',
+      data: { renderMode: RenderMode.Client },
+      loadComponent: () =>
+        import(
+          './pages/projects.component/projects-details.component/projects-details.component'
+        ).then((m) => m.ProjectsDetailsComponent),
+      
+    },
+    { path: 'contact', component: ContactComponent },
+    { path: 'donations', component: DonationsComponent },
+    { path: 'projects/charla-micaela-bastidas', component: HomeComponent },
+  ];
