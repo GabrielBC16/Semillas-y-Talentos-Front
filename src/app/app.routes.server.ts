55b52ad90061f4aa77debeb9f2de4432dcf027fd
Gabriel Barrantes <137751722+GabrielBC16@users.noreply.github.com>
2025-11-11 14:22:10 -0500

    rutas corregidas

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,13 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { get } from 'http';
 
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
     renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'projects/:id',
+    renderMode: RenderMode.Client
   }
 ];
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,22 +4,15 @@
   import { ProjectsComponent } from './pages/projects.component/projects.component';
   import { ContactComponent } from './pages/contact.component/contact.component';
   import { DonationsComponent } from './pages/donations.component/donations.component';
-import { RenderMode } from '@angular/ssr';
+  import { RenderMode } from '@angular/ssr';
+import path from 'path';
+import { ProjectsDetailsComponent } from './pages/projects.component/projects-details.component/projects-details.component';
 
   export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'about', component: AboutComponent },
     { path: 'projects', component: ProjectsComponent },
-    {
-      path: 'projects/:id',
-      data: { renderMode: RenderMode.Client },
-      loadComponent: () =>
-        import(
-          './pages/projects.component/projects-details.component/projects-details.component'
-        ).then((m) => m.ProjectsDetailsComponent),
-      
-    },
+    {path: 'projects/:id', component: ProjectsDetailsComponent},
     { path: 'contact', component: ContactComponent },
     { path: 'donations', component: DonationsComponent },
-    { path: 'projects/charla-micaela-bastidas', component: HomeComponent },
   ];
